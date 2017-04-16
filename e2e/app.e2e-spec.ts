652c74a729ad8ef9c2b1185f8ca3b5068a97fde6
Paul Lockwood <dotnetchap@gmail.com>
2017-04-16 10:26:51 -0600

    Checkpoint: angular-cli@1.0.0-beta24 working + has yarn.lock
    user@VM-LTS16:~/Code/Angular-CLI-Font-Awesome-Simple-Demo$ ng -v
    angular-cli: 1.0.0-beta.24
    node: 6.10.2
    os: linux x64
    @angular/common: 2.4.10
    @angular/compiler: 2.4.10
    @angular/core: 2.4.10
    @angular/forms: 2.4.10
    @angular/http: 2.4.10
    @angular/platform-browser: 2.4.10
    @angular/platform-browser-dynamic: 2.4.10
    @angular/router: 3.4.10
    @angular/compiler-cli: 2.4.10

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { FaTestPage } from './app.po';
+
+describe('fa-test App', function() {
+  let page: FaTestPage;
+
+  beforeEach(() => {
+    page = new FaTestPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
